bd832033730a5a3d2361143f5583615c4a03cdbe
OferTrabajo <92343204+OferTrabajo@users.noreply.github.com>
2024-11-05 11:42:57 -0500

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -330,33 +330,36 @@
 	});
 
 	const d = 40;
-document.querySelectorAll('.rocket-button').forEach(elem => {
-    elem.querySelectorAll('.default, .success > div').forEach(text => {
-        charming(text);
-        text.querySelectorAll('span').forEach((span, i) => {
-            span.innerHTML = span.textContent == ' ' ? '&nbsp;' : span.textContent;
-            span.style.setProperty('--d', i * d + 'ms');
-            span.style.setProperty('--ds', text.querySelectorAll('span').length * d - d - i * d + 'ms');
-        });
-    });
-
-    elem.addEventListener('click', e => {
-        e.preventDefault();
-
-        if (elem.classList.contains('animated')) {
-            return;
-        }
-
-        elem.classList.add('animated');
-        elem.classList.toggle('live');
-
-        setTimeout(() => {
-            elem.classList.remove('animated');
-            // Redirigir después de la animación
-            window.location.href = "rocket.html"; // Reemplaza con la URL de destino
-        }, 2400); // Ajustar según la duración de la animación
-    });
-});
+	document.querySelectorAll('.rocket-button').forEach(elem => {
+		elem.querySelectorAll('.default, .success > div').forEach(text => {
+			charming(text);
+			text.querySelectorAll('span').forEach((span, i) => {
+				span.innerHTML = span.textContent == ' ' ? '&nbsp;' : span.textContent;
+				span.style.setProperty('--d', i * d + 'ms');
+				span.style.setProperty('--ds', text.querySelectorAll('span').length * d - d - i * d + 'ms');
+			});
+		});
+	
+		elem.addEventListener('click', e => {
+			e.preventDefault();
+	
+			if (elem.classList.contains('animated')) {
+				return;
+			}
+	
+			elem.classList.add('animated');
+			elem.classList.toggle('live');
+	
+			setTimeout(() => {
+				elem.classList.remove('animated');
+				// Obtener la URL específica del botón
+				const url = elem.getAttribute('data-url');
+				if (url) {
+					window.location.href = url;
+				}
+			}, 2400); // Ajustar según la duración de la animación
+		});
+	});
 document.querySelectorAll('.button').forEach(button => {
     let getVar = variable => getComputedStyle(button).getPropertyValue(variable);
     button.addEventListener('click', e => {
@@ -465,5 +468,8 @@ document.querySelectorAll('.button').forEach(button => {
         }
     });
 });
+
+
+
 })();
 
