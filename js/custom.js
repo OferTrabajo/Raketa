ebec957fbf1ed402bf78d35cafc25dc34b0694c8
OferTrabajo <92343204+OferTrabajo@users.noreply.github.com>
2024-11-04 22:35:06 -0500

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -329,8 +329,141 @@
 		portfolioMasonry();
 	});
 
-	
+	const d = 40;
+document.querySelectorAll('.rocket-button').forEach(elem => {
+    elem.querySelectorAll('.default, .success > div').forEach(text => {
+        charming(text);
+        text.querySelectorAll('span').forEach((span, i) => {
+            span.innerHTML = span.textContent == ' ' ? '&nbsp;' : span.textContent;
+            span.style.setProperty('--d', i * d + 'ms');
+            span.style.setProperty('--ds', text.querySelectorAll('span').length * d - d - i * d + 'ms');
+        });
+    });
 
+    elem.addEventListener('click', e => {
+        e.preventDefault();
 
+        if (elem.classList.contains('animated')) {
+            return;
+        }
+
+        elem.classList.add('animated');
+        elem.classList.toggle('live');
+
+        setTimeout(() => {
+            elem.classList.remove('animated');
+            // Redirigir después de la animación
+            window.location.href = "rocket.html"; // Reemplaza con la URL de destino
+        }, 2400); // Ajustar según la duración de la animación
+    });
+});
+document.querySelectorAll('.button').forEach(button => {
+    let getVar = variable => getComputedStyle(button).getPropertyValue(variable);
+    button.addEventListener('click', e => {
+        if (!button.classList.contains('active')) {
+            button.classList.add('active');
+            gsap.to(button, {
+                keyframes: [{
+                    '--left-wing-first-x': 50,
+                    '--left-wing-first-y': 100,
+                    '--right-wing-second-x': 50,
+                    '--right-wing-second-y': 100,
+                    duration: .2,
+                    onComplete() {
+                        gsap.set(button, {
+                            '--left-wing-first-y': 0,
+                            '--left-wing-second-x': 40,
+                            '--left-wing-second-y': 100,
+                            '--left-wing-third-x': 0,
+                            '--left-wing-third-y': 100,
+                            '--left-body-third-x': 40,
+                            '--right-wing-first-x': 50,
+                            '--right-wing-first-y': 0,
+                            '--right-wing-second-x': 60,
+                            '--right-wing-second-y': 100,
+                            '--right-wing-third-x': 100,
+                            '--right-wing-third-y': 100,
+                            '--right-body-third-x': 60
+                        });
+                    }
+                }, {
+                    '--left-wing-third-x': 20,
+                    '--left-wing-third-y': 90,
+                    '--left-wing-second-y': 90,
+                    '--left-body-third-y': 90,
+                    '--right-wing-third-x': 80,
+                    '--right-wing-third-y': 90,
+                    '--right-body-third-y': 90,
+                    '--right-wing-second-y': 90,
+                    duration: .2
+                }, {
+                    '--rotate': 50,
+                    '--left-wing-third-y': 95,
+                    '--left-wing-third-x': 27,
+                    '--right-body-third-x': 45,
+                    '--right-wing-second-x': 45,
+                    '--right-wing-third-x': 60,
+                    '--right-wing-third-y': 83,
+                    duration: .25
+                }, {
+                    '--rotate': 55,
+                    '--plane-x': -8,
+                    '--plane-y': 24,
+                    duration: .2
+                }, {
+                    '--rotate': 40,
+                    '--plane-x': 45,
+                    '--plane-y': -180,
+                    '--plane-opacity': 0,
+                    duration: .3,
+                    onComplete() {
+                        setTimeout(() => {
+                            button.removeAttribute('style');
+                            gsap.fromTo(button, {
+                                opacity: 0,
+                                y: -8
+                            }, {
+                                opacity: 1,
+                                y: 0,
+                                clearProps: true,
+                                duration: .3,
+                                onComplete() {
+                                    button.classList.remove('active');
+                                    // Redirigir a la URL específica
+                                    const url = button.getAttribute('data-url');
+                                    if (url) {
+                                        window.location.href = url;
+                                    }
+                                }
+                            });
+                        }, 2000);
+                    }
+                }]
+            });
+            gsap.to(button, {
+                keyframes: [{
+                    '--text-opacity': 0,
+                    '--border-radius': 0,
+                    '--left-wing-background': getVar('--primary-darkest'),
+                    '--right-wing-background': getVar('--primary-darkest'),
+                    duration: .1
+                }, {
+                    '--left-wing-background': getVar('--primary'),
+                    '--right-wing-background': getVar('--primary'),
+                    duration: .1
+                }, {
+                    '--left-body-background': getVar('--primary-dark'),
+                    '--right-body-background': getVar('--primary-darkest'),
+                    duration: .4
+                }, {
+                    '--success-opacity': 1,
+                    '--success-scale': 1,
+                    duration: .25,
+                    delay: .25
+                }]
+            });
+        }
+    });
+});
 })();
 
